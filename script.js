b0ba588c6d8b61f11f4837193cd1b95fdcb0faf4
MasterSergeant98 <31738880+MasterSergeant98@users.noreply.github.com>
2017-09-26 01:26:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 $(function(){
 	var carouselList = $("#carousel ul");
-	setTimeout(changeSlide, 5000); //after 3 seconds will change the slide once
-	setInterval(changeSlide, 5000); //every 3 seconds, it performs a function to change the slide
+	setInterval(changeSlide, 5000); 
 	carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
-	function.moveFirstSlide{
-	var firstItem = carouselList.find("li:first");
-	var lastItem = carouselList.find("li:last");
-	lastItem.after(firstItem);
-	carouselList.css({marginLeft:0});
-}
+	
+    
+	moveFirstSlide = function(){
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	}
 });
+
